cf0d950234e31a9783b7c070c50fc7c6b835e622
mayank123904 <mj2391791@gmail.com>
2023-05-27 17:29:54 +0530

    DB URI Changed

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = () => {
   mongoose
-    .connect(process.env.MONGO_URI, {
+    .connect(process.env.DB_URI, {
       dbName: "backendapi",
     })
     .then((c) => console.log(`Database Connected with ${c.connection.host}`))
