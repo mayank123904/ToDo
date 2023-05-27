fde4932af4887e6b1b2251af38a4e4c1bcc4da52
mayank123904 <mj2391791@gmail.com>
2023-05-27 16:18:51 +0530

    my commit

--- controllers/taskcontroller.js
+++ controllers/taskcontroller.js
@@ -1,72 +1,62 @@
+import { User } from "../models/user.js";
+import bcrypt from "bcrypt";
+import { sendCookie } from "../utils/features.js";
 import ErrorHandler from "../middlewares/error.js";
-import  {Task} from "../models/taskmodel.js";
 
-export const newTask = async(req,res,next) => {
- try {
- const {title,description}=req.body;
- 
- const task=new Task({title});
- await Task.create({
-  title,
-  description,
-  user:req.user,
- });
-  res.status(201).json({
-  success:true,
-  message:"Task added Successfully",
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) return next(new ErrorHandler("Invalid Email or Password", 400));
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch)
+      return next(new ErrorHandler("Invalid Email or Password", 400));
+
+    sendCookie(user, res, `Welcome back, ${user.name}`, 200);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    let user = await User.findOne({ email });
+
+    if (user) return next(new ErrorHandler("User Already Exist", 400));
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    user = await User.create({ name, email, password: hashedPassword });
+
+    sendCookie(user, res, "Registered Successfully", 201);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getMyProfile = (req, res) => {
+  res.status(200).json({
+    success: true,
+    user: req.user,
   });
- } catch (error) {
-    next(error);
- } 
 };
 
-
-export const getMyTask =  async(req,res,next) => {
-try {
-    const userid=req.user._id;
-const tasks=await Task.find({user:userid});
-
-res.status(200).json({
-success:true,
-tasks,
-});
-} catch (error) {
-    next(error);
-}
-};
-
-export const updateTask =  async(req,res,next) => {
-    try {
-        const task=await Task.findById(req.params.id);
-    
-        if(!task) return next(new ErrorHandler("Task not found",404));
-    
-        task.isCompleted=!task.isCompleted;
-        await task.save();
-    
-        res.status(200).json({
-        success:true,
-        message:"Task Updated!",
-        }); 
-    } catch (error) {
-        next(error);
-    }
- 
-};
-
-export const deleteTask =  async(req,res,next) => {
-    try {
-        const task=await Task.findById(req.params.id);
-
-        if(!task) return next(new ErrorHandler("Task not found",404));
-        await task.deleteOne();
-    
-        res.status(200).json({
-        success:true,
-        message:"Task Deleted!",
-        });
-    } catch (error) {
-        next(error);
-    }
-   
+export const logout = (req, res) => {
+  res
+    .status(200)
+    .cookie("token", "", {
+      expires: new Date(Date.now()),
+      sameSite: process.env.NODE_ENV === "Develpoment" ? "lax" : "none",
+      secure: process.env.NODE_ENV === "Develpoment" ? false : true,
+    })
+    .json({
+      success: true,
+      user: req.user,
+    });
 };
--- database/db.js
+++ database/db.js
@@ -1,11 +1,10 @@
 import mongoose from "mongoose";
 
-export const connectDB=()=>{
-    mongoose
-    .connect(process.env.DB_URI, {
-      dbName: "Backendapi",
+export const connectDB = () => {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      dbName: "backendapi",
     })
-    .then(() => console.log("Database Connected"))
+    .then((c) => console.log(`Database Connected with ${c.connection.host}`))
     .catch((e) => console.log(e));
-  
 };
