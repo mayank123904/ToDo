5ecdb5674d0cea2963e8e0b79d43ad3fe922fbbd
mayank123904 <mj2391791@gmail.com>
2023-05-27 16:47:52 +0530

    internal changes

--- controllers/taskcontroller.js
+++ controllers/taskcontroller.js
@@ -1,62 +1,70 @@
-import { User } from "../models/user.js";
-import bcrypt from "bcrypt";
-import { sendCookie } from "../utils/features.js";
 import ErrorHandler from "../middlewares/error.js";
+import { Task } from "../models/taskmodel.js";
 
-export const login = async (req, res, next) => {
+export const newTask = async (req, res, next) => {
   try {
-    const { email, password } = req.body;
+    const { title, description } = req.body;
 
-    const user = await User.findOne({ email }).select("+password");
-
-    if (!user) return next(new ErrorHandler("Invalid Email or Password", 400));
-
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    if (!isMatch)
-      return next(new ErrorHandler("Invalid Email or Password", 400));
-
-    sendCookie(user, res, `Welcome back, ${user.name}`, 200);
-  } catch (error) {
-    next(error);
-  }
-};
-
-export const register = async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
-
-    let user = await User.findOne({ email });
-
-    if (user) return next(new ErrorHandler("User Already Exist", 400));
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    user = await User.create({ name, email, password: hashedPassword });
-
-    sendCookie(user, res, "Registered Successfully", 201);
-  } catch (error) {
-    next(error);
-  }
-};
-
-export const getMyProfile = (req, res) => {
-  res.status(200).json({
-    success: true,
-    user: req.user,
-  });
-};
-
-export const logout = (req, res) => {
-  res
-    .status(200)
-    .cookie("token", "", {
-      expires: new Date(Date.now()),
-      sameSite: process.env.NODE_ENV === "Develpoment" ? "lax" : "none",
-      secure: process.env.NODE_ENV === "Develpoment" ? false : true,
-    })
-    .json({
-      success: true,
+    await Task.create({
+      title,
+      description,
       user: req.user,
     });
+
+    res.status(201).json({
+      success: true,
+      message: "Task added Successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getMyTask = async (req, res, next) => {
+  try {
+    const userid = req.user._id;
+
+    const tasks = await Task.find({ user: userid });
+
+    res.status(200).json({
+      success: true,
+      tasks,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateTask = async (req, res, next) => {
+  try {
+    const task = await Task.findById(req.params.id);
+
+    if (!task) return next(new ErrorHandler("Task not found", 404));
+
+    task.isCompleted = !task.isCompleted;
+    await task.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Task Updated!",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteTask = async (req, res, next) => {
+  try {
+    const task = await Task.findById(req.params.id);
+
+    if (!task) return next(new ErrorHandler("Task not found", 404));
+    await task.deleteOne();
+
+    res.status(200).json({
+      message: "Task Deleted!",
+      success: true,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -2,64 +2,61 @@ import { User } from "../models/usermodel.js";
 import bcrypt from "bcrypt";
 import { sendCookie } from "../utils/features.js";
 import ErrorHandler from "../middlewares/error.js";
-import next from "next";
 
-export const login=async(req,res,next)=>{
-try {
-  const {email,password}=req.body;
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
 
-  const user=await User.findOne({email}).select("+password");
-  
-  if(!user) return next(new ErrorHandler("Invalid Email or Password",400));
+    const user = await User.findOne({ email }).select("+password");
 
-  const isMatch=await bcrypt.compare(password,user.password);
+    if (!user) return next(new ErrorHandler("Invalid Email or Password", 400));
 
-  if(!isMatch) return next(new ErrorHandler("Invalid Email or Password",400));
-  
-  sendCookie(user,res,`Welcome back, ${user.name}`,200)
+    const isMatch = await bcrypt.compare(password, user.password);
 
-  
-} catch (error) {
-  next(error);
-}
- };
+    if (!isMatch)
+      return next(new ErrorHandler("Invalid Email or Password", 400));
 
-export const register=async(req,res)=>{
-try {
-  const {name,email,password}=req.body;
-
-let user=await User.findOne({email});
-
-if(user) return next(new ErrorHandler("User Already Exist",400));
-
-const hashedPassword=await bcrypt.hash(password,10);
-
-user=await User.create({
-name,
-email,
-password:hashedPassword,
-});
-
-sendCookie(user,res,"Registered Successfully,201");
-} catch (error) {
-  next(error);
-}
+    sendCookie(user, res, `Welcome back, ${user.name}`, 200);
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const getMyProfile=async(req,res)=>{   //if we use /userid/:id it will act as dynamic
+export const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    let user = await User.findOne({ email });
+
+    if (user) return next(new ErrorHandler("User Already Exist", 400));
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    user = await User.create({ name, email, password: hashedPassword });
+
+    sendCookie(user, res, "Registered Successfully", 201);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getMyProfile = (req, res) => {
   res.status(200).json({
-    success:true,
-    user:req.user,
+    success: true,
+    user: req.user,
   });
 };
 
-export const logout=async(req,res)=>{
-  res.status(200).cookie("token","",{
-    expires:new Date(Date.now()),
-    sameSite:process.env.NODE_ENV==="Development" ? "lax" : "none",
-      secure:process.env.NODE_ENV==="Development" ? false : true,
-  }).json({
-    success:true,
-    message:"Logged Out Successfully",
+export const logout = (req, res) => {
+  res
+    .status(200)
+    .cookie("token", "", {
+      expires: new Date(Date.now()),
+      sameSite: process.env.NODE_ENV === "Develpoment" ? "lax" : "none",
+      secure: process.env.NODE_ENV === "Develpoment" ? false : true,
+    })
+    .json({
+      success: true,
+      user: req.user,
     });
 };
